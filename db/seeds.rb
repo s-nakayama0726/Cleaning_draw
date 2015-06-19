1db0e2b5ccae6f413fa602f07fd71b18c6c19c6c
s-nakayama0726 <s-nakayama@ankh-systems.co.jp>
2015-06-19 14:48:30 +0900

    テスト用のテーブルデータdb\seeds.rb更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# coding: utf-8
+
+user = CleaningEntry.new
+user.name = "Aさん"
+user.draw_no = 1;
+user.join_flag = 1;
+user.save
+
+user = CleaningEntry.new
+user.name = "Bさん"
+user.draw_no = 2;
+user.join_flag = 1;
+user.save
+
+user = CleaningEntry.new
+user.name = "Cさん"
+user.draw_no = 3;
+user.join_flag = 0;
+user.save
+
+user = CleaningEntry.new
+user.name = "Dさん"
+user.draw_no = 4;
+user.join_flag = 0;
+user.save
+
+user = CleaningEntry.new
+user.name = "Eさん"
+user.draw_no = 5;
+user.join_flag = 0;
+user.save
