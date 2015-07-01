74ca627780c7f50a4109a42437fe4bed7c256347
s-nakayama0726 <s-nakayama@ankh-systems.co.jp>
2015-07-01 17:26:14 +0900

    MailActionテスト

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:               'smtp.gmail.com',
+  port:                  587,
+  domain:                '',
+  user_name:             'shingood98@gmail.com',
+  password:              'hikariww2000',
+  authentication:        'plain',
+  enable_starttls_auto:  true
+  }
 end
