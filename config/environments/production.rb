0a11bc8a036cce47452e9ba5a291b230840f1486
s-nakayama0726 <s-nakayama@ankh-systems.co.jp>
2015-08-07 11:21:33 +0900

    ・メール設定変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,11 +79,11 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  address:               'ankh-systems.co.jp',
+  address:               'smtp.gmail.com',
   port:                  587,
   domain:                '',
-  user_name:             's-nakayama',
-  password:              'Beekay39',
+  user_name:             'shingood98@gmail.com',
+  password:              'hikariww2000',
   authentication:        'plain',
   enable_starttls_auto:  true
   }
