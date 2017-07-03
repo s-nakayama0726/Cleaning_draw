6eb20197cb68d04dd7f64889d7eec9551bbb8caf
tkwon <tkwon@example.com>
2017-07-03 10:47:22 +0900

    gmail利用

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,12 +79,12 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  address:               'mail.ankh-systems.co.jp',
-  port:                  587,
-  domain:                'ankh-systems.com',
-  user_name:             's-nakayama@ankh-systems.co.jp',
-  password:              'Beekay39',
-  authentication:        'plain',
-  enable_starttls_auto:  true
+    :enable_starttls_auto => true,
+    :address => 'smtp.gmail.com',
+    :port => '587',
+    :domain => 'smtp.gmail.com',
+    :authentication => 'plain',
+    :user_name => 'tehoankh@gmail.com',
+    :password => 'Ankh1111'
   }
 end
